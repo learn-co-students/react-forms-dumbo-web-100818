344c5fc7482b546c5a8c01235cdd0dc2109b2365
github username <github email address>
2018-12-12 13:38:20 -0500

    Done.

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export default class Form extends React.Component{
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  }
+
+  handleFirstNameChange =(e) => {
+    this.setState({
+      firstName: e.target.value
+    })
+  }
+
+  handleLastNameChange = e => {
+    this.setState({
+      lastName: e.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const firstName = event.target.children[0].value
+    const lasstName = event.target.children[1].value
+    // this.sendFormDataSomewhere({ firstName, lastName })
+  }
+
+
+  render(){
+    return(
+      <form onSubmit={ event => this.handleSubmit(event) }>
+      <input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+      <input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+      </form>
+    )
+  }
+}
