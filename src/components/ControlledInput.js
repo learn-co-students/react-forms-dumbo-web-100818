f9bec9fbc11b53f5a23328b7b583b4e01b097fa7
github username <github email address>
2018-12-13 20:15:01 -0500

    Done.

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,28 @@
-// Code ControlledInput Component Here
+import React, { Component } from 'react';
+
+class ControlledInput extends Component {
+
+  state ={
+    value: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+// the value is for setting the state to equal what is in the states value. onChange is listening for any changes made to the form and sets the state with a function
+  render() {
+    return (
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+
+}
+
+export default ControlledInput;
